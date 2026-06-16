ec54967871a72520ceecca6bbe00447174530deb
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2026-06-16 11:34:55 +0530

    Update providers.tf

--- AKS/providers.tf
+++ AKS/providers.tf
@@ -1,15 +1,18 @@
 terraform {
+  required_version = "1.15.6"
+
+  cloud {
+    organization = "pavankattamuri"
+
+    workspaces {
+      name = "poc-15"
+    }
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
-    }
-  }
-  backend "remote" {
-    organization = "pavankattamuri"
-
-    workspaces {
-      name = "azure-aks-deployment"
     }
   }
 }
